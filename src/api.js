9970153376a68eb1372faef80fa3eb8815061b60
MD MAHBUB REZA <141657720+mdmahbubreza@users.noreply.github.com>
2025-11-15 16:07:33 +0530

    Delete src directory

--- src/api.js
+++
@@ -1,6 +0,0 @@
-export const fetchBooks = async (query = '', page = 1, limit = 10) => {
-    const response = await fetch(`https://openlibrary.org/search.json?q=${query}&page=${page}&limit=${limit}`);
-    const data = await response.json();
-    return data;
-  };
-  
